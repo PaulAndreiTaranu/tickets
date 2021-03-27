7245ddb7a34a3918df06b827e0fe3b0d721f58e1
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2021-03-27 13:21:57 +0000

    deploying

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-publisher"
-import { TicketCreatedEvent } from "./ticket-create-event"
-import { Subjects } from "./subjects"
-
-export class TicketCreatedPubliser extends Publisher<TicketCreatedEvent> {
-    subject: Subjects.TicketCreated = Subjects.TicketCreated
-}
